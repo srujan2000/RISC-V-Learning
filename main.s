9f954de2075ca1c5b1d8fc5e8132612f5801eb16
srujan2000 <srujanreddy168@gmail.com>
2025-06-22 14:45:59 +0530

    added uart message

--- main.s
+++ main.s
@@ -6,5 +6,22 @@ main:
   addi x2, x2, 5
   addi x1, x2, 5
   csrr t0, mcause
+  addi a0, x0, 0x41
+	li a1, 0x10000000
+	sb a0, (a1) # 'a'
+  addi a0, x0, 0x42
+	li a1, 0x10000000
+	sb a0, (a1) # 'b'
+  addi a0, x0, 0x43
+	li a1, 0x10000000
+	sb a0, (a1) # 'c'
+  addi a0, x0, 0x44
+	li a1, 0x10000000
+	sb a0, (a1) # 'd'
+  addi a0, x0, 0x45
+	li a1, 0x10000000
+	sb a0, (a1) # 'e'
+
+loop: j loop
 
 
